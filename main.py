c271b10d52b8f7221f802ef4a0058cb342c94d37
5ham <surajrxl06@gmail.com>
2026-03-03 17:39:27 +0530

    A cli prototype with csv file and manual url shortenign and getting

--- main.py
+++ main.py
@@ -2,14 +2,39 @@ from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.backends import default_backend
 import base64
-import json
 import os
 import csv
 
 
+def databaseReading () :
+    try:
+        if os.path.exists("urlDatabase.csv"):
+            urlData = []
+            with open("urlDatabase.csv", "r") as f:
+                reader = csv.DictReader(f)
+                for row in reader :
+                    urlData.append(row)
+                return urlData
+    except FileNotFoundError :
+        print("File not found")
+        return None
+    
 
-url = input("Enter a url : ").encode()
+def urlChecking (url) :
+    data = databaseReading()
+    if (data == None) :
+        return None
+    for i in range(0,len(data),1):
+        dataUrl = data[i]["url"]
+        dataEncryptedUrl = data[i]["encryptedUrl"]
+        if (dataUrl == url) :
+            return dataEncryptedUrl
+    return None
+
+        
+
 def urlShortening () :
+    url = input("Enter a url : ").encode()
     salt = os.urandom(4)
 
     kdf = PBKDF2HMAC(
@@ -20,20 +45,65 @@ def urlShortening () :
         backend=default_backend()
     )
     encryptedUrl = base64.urlsafe_b64encode(kdf.derive(url)).rstrip(b"=")
-    return encryptedUrl
-
+    return encryptedUrl, url 
 
 
 def databaseWriting () :
-    encryptedUrl = urlShortening()
-    if (os.path.exists("urlDatabase.csv")) :
-        with open("urlDatabase.csv", "a", newline="")as file:
-            writer = csv.DictWriter(file, fieldnames=["encryptedUrl", "url"])
-            writer.writerow({"encryptedUrl":encryptedUrl.decode(),"url":url.decode()})
+    encryptedUrl, url = urlShortening()
+    urlCheck = urlChecking(url.decode())
+    link = "https://5ham/"
+    if not urlCheck:
+        print(f"\nShortened Link :- {link + encryptedUrl.decode()}")          
+        if (os.path.exists("urlDatabase.csv")) :
+            with open("urlDatabase.csv", "a", newline="")as file:
+                writer = csv.DictWriter(file, fieldnames=["encryptedUrl", "url"])
+                writer.writerow({"encryptedUrl":encryptedUrl.decode(),"url":url.decode()})
+        else :
+            with open("urlDatabase.csv", "w", newline="")as file:
+                writer = csv.DictWriter(file, fieldnames=["encryptedUrl", "url"])
+                writer.writeheader()
+                writer.writerow({"encryptedUrl":encryptedUrl.decode(),"url":url.decode()})
     else :
-        with open("urlDatabase.csv", "w", newline="")as file:
-            writer = csv.DictWriter(file, fieldnames=["encryptedUrl", "url"])
-            writer.writeheader()
-            writer.writerow({"encryptedUrl":encryptedUrl.decode(),"url":url.decode()})
+        print(f"\nShortened Link :- {link+urlCheck}")
+
+
+def gettingUrl() :
+    data = databaseReading()
+    if (not data) :
+        return None
+    shortenedUrl = input("Enter your shortened Url : ")
+    encrypedUrl = shortenedUrl.removeprefix("https://5ham/")
+    for i in range(0,len(data),1):
+        dataUrl = data[i]["url"]
+        dataEncryptedUrl = data[i]["encryptedUrl"]
+        if (dataEncryptedUrl == encrypedUrl) :
+            return dataUrl
+    return None
+
+    
+while True:
+    print()
+    print("-"*100)
+    print("URL Shortener")
+    print("-"*100)
+    print("\n1.Shorten your URL")
+    print('2.Get Your Original URL')
+    print("3.Exit the Program\n")
+    
+    try :
+        choice = int(input("\nEnter your choice (1/2/3) : "))
+        if choice not in range(1,4) :
+            raise ValueError
+    except ValueError as e :
+        print("\nInvalid Input ",e)
+    if choice == 1:
+        databaseWriting()
+    elif choice == 2 :
+        yourUrl = gettingUrl()
+        if (yourUrl) :
+            print(f"\nYour Original URL :- {yourUrl}")
+    elif choice == 3 :
+        break
+
 
 
