808aec36c7ed9aafea58ac01f2d629357f426568
5ham <surajrxl06@gmail.com>
2026-03-22 16:19:42 +0530

    Committing entire backend for this website

--- main.py
+++ main.py
@@ -1,96 +1,86 @@
-import os
-import csv
+from flask import Flask, jsonify, redirect, url_for, request
+from flask_cors import CORS
 import string
-import time
 import sqlite3
 
-domain_name = "https://5h-am.com/"
+app = Flask(__name__)
+CORS(app)
+
+
+
+domain_name = "http://127.0.0.1:5000/5ham/"
+
+def init_db():
+    with sqlite3.connect("database.db") as conn:
+        conn.execute("""
+        CREATE TABLE IF NOT EXISTS url_records (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            short_url TEXT UNIQUE,
+            url TEXT NOT NULL
+        )
+        """)
+
+init_db()
+
+@app.route("/")
+def home():
+    return jsonify({"message": "Backend working!"})
+
 def databaseReading (short_url) :
     try:
         with sqlite3.connect("database.db")as conn:
             cursor = conn.cursor()
-            cursor.execute("SELECT url FROM url_records WHERE short_url == ?",(short_url,))
+            cursor.execute("SELECT url FROM url_records WHERE short_url = ?",(short_url,))
             result = cursor.fetchone()
             if result:
                 return result[0]
             return None
-    except FileNotFoundError :
-        print("File not found")
+    except sqlite3.OperationalError :
         return None
     
 
-
-
-def counter_reading () :
-    if os.path.exists("database.db"):
-        with sqlite3.connect("database.db") as conn:
-            cursor = conn.cursor()
-            cursor.execute("SELECT MAX(id) FROM url_records")
-            count = cursor.fetchone()[0]
-            return count
-    else :
-        return 0  
-
-def urlShortening () :
-    url = input("Enter a url : ")
-    if not url.startswith("https://") :
-        print("Error, Only URLs are allowed")
-        return None, None
-    count = counter_reading()
-    count += 1
+    
+def base62encoding(count):
     temp = []
     base62 = string.digits+string.ascii_lowercase+string.ascii_uppercase
+    if count == 0:
+        return base62[0]
     num = count
     while (num > 0) :
         num, rem = divmod(num,62)
         temp.append(base62[rem])
-    encryptedUrl = "".join(reversed(temp)) 
-    return encryptedUrl, url 
+    encodedUrl = "".join(reversed(temp)) 
+    return encodedUrl
+
+@app.route("/NotFound")
+def notFound():
+    return "<h2>Url Not Found</h2>"
+
+@app.route("/5ham/<string:shortUrl>")
+def gettingUrl(shortUrl) :   
+    data = databaseReading(shortUrl)
+    if not data:
+        return redirect(url_for("notFound")) 
+    return  redirect(data)
 
 
-def databaseWriting () :
-    encryptedUrl, url = urlShortening()
-    if encryptedUrl:
-        print(f"\nShortened Link :- {domain_name+ encryptedUrl}")          
-        with sqlite3.connect("database.db") as conn:
-            cursor = conn.cursor()
-            conn.execute("INSERT INTO url_records (short_url, url) VALUES(?,?)",(encryptedUrl,url))
-            conn.commit()
+@app.route("/shorten", methods=["POST"])
+def urlShortening () :
+    data = request.get_json()
+    if not data or "url" not in data:
+        return jsonify({"error": "Missing URL"}), 400
+    url = data.get("url")
+    if not url.startswith(("http://", "https://")):
+        return jsonify({"error": "Invalid URL"}), 400
+    with sqlite3.connect("database.db") as conn:
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO url_records (url) VALUES (?)",(url,))
+        id_value = cursor.lastrowid
+        short_code = base62encoding(id_value)
+        cursor.execute("UPDATE url_records SET short_url=? WHERE id=?",(short_code, id_value))
+        conn.commit()
+        return jsonify({"shortenedUrl":short_code})
 
-def gettingUrl() :   
-    shortenedUrl = input("Enter your shortened Url : ")
-    encryptedUrl = shortenedUrl.removeprefix(domain_name)
-    data = databaseReading(encryptedUrl)
-    return data
- 
-while True:
-    print()
-    print("-"*100)
-    print("URL Shortener")
-    print("-"*100)
-    print("\n1.Shorten your URL")
-    print('2.Get Your Original URL')
-    print("3.Exit the Program\n")
-    
-    try :
-        choice = int(input("\nEnter your choice (1/2/3) : "))
-        if choice not in range(1,4) :
-            raise ValueError
-    except ValueError as e :
-        print("\nInvalid Input ",e)
-        continue 
-    if choice == 1:
-        databaseWriting()
-        time.sleep(1)      
-    elif choice == 2 :
-        yourUrl = gettingUrl()
-        if (yourUrl) :
-            print(f"\nYour Original URL :- {yourUrl}")
-        else :
-            print("\nSorry, This doesn't match with any orignal link")
-        time.sleep(1)
-    elif choice == 3 :
-        print("\nGoodbye, Have a nice day")
-        break
+
 
 
